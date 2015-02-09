9a5c2ba9fb9806002519d6bb83f99dbc069d5bd9
ByteBlast <byteblast@gmail.com>
2015-02-09 23:33:21 +0000

    Added account controller

---
+++ src/Twitter/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+
+namespace Twitter.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
