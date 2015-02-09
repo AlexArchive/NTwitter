35d352cea9f8bfd1c7c7e9ed870230f322117679
ByteBlast <byteblast@gmail.com>
2015-02-09 23:07:44 +0000

    Initial commit

---
+++ src/Twitter/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace Twitter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.WelcomeMessage = "Hello, all.";
+            return View();
+        }
+    }
+}
