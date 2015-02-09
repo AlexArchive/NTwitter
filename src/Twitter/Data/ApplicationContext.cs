1265a331e6c94d0c7a6b6ee4352ead85a6aeee4a
ByteBlast <byteblast@gmail.com>
2015-02-09 23:51:03 +0000

    Added database foundation

---
+++ src/Twitter/Data/ApplicationContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using Twitter.Data.Model;
+
+namespace Twitter.Data
+{
+    public class ApplicationContext : IdentityDbContext<ApplicationUser>
+    {
+    }
+}
---
+++ src/Twitter/Data/Model/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity;
+
+namespace Twitter.Data.Model
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
