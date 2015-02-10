06148e56a41b27d6d52b518f090f148aead46c5b
ByteBlast <byteblast@gmail.com>
2015-02-10 23:15:13 +0000

    Added test foundation

---
+++ tests/Twitter.UnitTest/HomeControllerFacts.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Mvc;
+using Twitter.Controllers;
+using Xunit;
+
+namespace Twitter.UnitTest
+{
+    public class HomeControllerFacts
+    {
+        [Fact]
+        public void SutIsController()
+        {
+            var sut = new HomeController();
+            Assert.IsAssignableFrom<Controller>(sut);
+        }
+
+        [Fact]
+        public void IndexReturnsCorrectView()
+        {
+            var sut = new HomeController();
+            var actual = (ViewResult) sut.Index();
+            Assert.Null(actual.ViewName);
+        }
+    }
+}
