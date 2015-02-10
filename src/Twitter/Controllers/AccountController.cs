a0f5936810f17ae971aa750d7b02b34f5228c8d5
ByteBlast <byteblast@gmail.com>
2015-02-10 16:57:40 +0000

    Added register view

--- src/Twitter/Controllers/AccountController.cs
+++ src/Twitter/Controllers/AccountController.cs
@@ -1,13 +1,27 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using Twitter.Models;
 
 namespace Twitter.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Register()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            throw new NotImplementedException();
+        }
+
     }
 }
