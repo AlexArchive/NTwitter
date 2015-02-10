15ab1e839fd3e9be5c9fe57e6f0a6b168f963872
ByteBlast <byteblast@gmail.com>
2015-02-10 20:10:46 +0000

    Added registration

--- src/Twitter/Controllers/AccountController.cs
+++ src/Twitter/Controllers/AccountController.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Mvc;
+using Twitter.Data.Model;
 using Twitter.Models;
 
 namespace Twitter.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
         public IActionResult Register()
         {
             return View();
@@ -16,12 +24,20 @@ namespace Twitter.Controllers
         public async Task<IActionResult> Register(RegisterModel model)
         {
             if (!ModelState.IsValid)
+                return View(model);
+            var user   = new ApplicationUser { UserName = model.Username };
+            var result = await userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
             {
+                ViewBag.Message = "Success";
                 return View();
             }
-
-            throw new NotImplementedException();
+            else
+            {
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError("", error);
+            }
+            return View(model);
         }
-
     }
 }
--- src/Twitter/Startup.cs
+++ src/Twitter/Startup.cs
@@ -1,17 +1,37 @@
 ﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Identity;
+using Microsoft.Framework.ConfigurationModel;
 using Microsoft.Framework.DependencyInjection;
+using Twitter.Data;
+using Twitter.Data.Model;
 
 namespace Twitter
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment environment)
+        {
+            Configuration = new Configuration()
+                .AddJsonFile("config.json")
+                .AddEnvironmentVariables();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddEntityFramework(Configuration)
+                .AddSqlServer()
+                .AddDbContext<ApplicationContext>();
+            services.AddIdentity<ApplicationUser, IdentityRole>(Configuration)
+                .AddEntityFrameworkStores<ApplicationContext>();
             services.AddMvc();
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseIdentity();
             app.UseMvc();
         }
     }
