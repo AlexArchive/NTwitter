0938ac5c42b7459a6746e21210fefa71db0e7137
ByteBlast <byteblast@gmail.com>
2015-02-10 20:34:57 +0000

    Added login support

--- src/Twitter/Controllers/AccountController.cs
+++ src/Twitter/Controllers/AccountController.cs
@@ -9,10 +9,14 @@ namespace Twitter.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager)
+        public AccountController(
+            UserManager<ApplicationUser> userManager, 
+            SignInManager<ApplicationUser> signInManager)
         {
             this.userManager = userManager;
+            this.signInManager = signInManager;
         }
 
         public IActionResult Register()
@@ -36,13 +40,36 @@ namespace Twitter.Controllers
             {
                 foreach (var error in result.Errors)
                     ModelState.AddModelError("", error);
+                return View(model);
             }
-            return View(model);
         }
 
         public IActionResult Login()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            var status = await signInManager.PasswordSignInAsync(
+                model.UserName, 
+                model.Password, 
+                model.RememberMe, 
+                false);
+            if (status == SignInStatus.Success)
+            {
+                ViewBag.Message = "Success";
+                return View();
+            }
+            else
+            {
+                ModelState.AddModelError("","Invalid username or password.");
+                return View(model);
+            }
+        }
+
     }
 }
