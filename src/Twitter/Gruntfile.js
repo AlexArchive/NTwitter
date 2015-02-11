3b643f5bd584bdd83260713c8c596d3db779f7d5
ByteBlast <byteblast@gmail.com>
2015-02-11 19:41:11 +0000

    Added Bootstrap etc.

---
+++ src/Twitter/Gruntfile.js
@@ -0,0 +1,17 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        bower: {
+            install: {
+                options: {
+                    targetDir: "wwwroot/lib",
+                    layout: "byComponent",
+                    cleanTargetDir: false
+                }
+            }
+        }
+    });
+
+    grunt.registerTask("default", ["bower:install"]);
+
+    grunt.loadNpmTasks("grunt-bower-task");
+};
