b0526164a28e948ae5a67b601d36d99c6bdee1c8
ByteBlast <byteblast@gmail.com>
2015-02-13 00:35:46 +0000

    Home redirects to register view if not authenticated

--- src/Twitter/Controllers/HomeController.cs
+++ src/Twitter/Controllers/HomeController.cs
@@ -4,6 +4,16 @@ namespace Twitter.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View();
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Register","Account");
+            }
+        }
     }
 }
